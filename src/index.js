2ffb0ff551d0f9574de3326ae51cc02a9c0c8561
Yusup Maulana <yusupmaulana950@gmail.com>
2018-05-03 22:46:07 +0700

    var vs let vs const

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-class Test {
+const person = {
+  id : 1,
+  name : 'Yusup'
+};
 
-}
+person.age = 21;
 
-console.log(Test);
+console.log(person);
+// var nama = "yusup"; //sifatnya function scope
+// let dan const sifatnya block scope
+// const tidak bisa di reassign (tp bukan immutable).
+// var dan let bisa reassign (tp let gabisa dekalarasikan ulang dengan nama yang sama)
+// function getName()
+// {
+//   if(false){
+//     let nama = "yusup";
+//     console.log(nama);
+//   }else {
+//     console.log('nama tidak ditemukan');
+//   }
+// }
+//
+// getName();
+// console.log(nama);
