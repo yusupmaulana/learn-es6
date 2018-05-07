a3324ce63da76fbf5e704e9bc64fe4097887fe96
Yusup Maulana <yusupmaulana950@gmail.com>
2018-05-07 11:55:12 +0700

    syntax arrow -- function shorthand

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-const person = {
+// ================================================================================
+// syntax arrow -- => -- function shorthand
+// var members = ['rizqi','dodi','qaiserlab','dodit'];
+// members.forEach((member,index) => {
+//     console.log(member + " " + index);
+// });
+
+let SekolahKoding = {
+  members : ['rizqi','dodi','qaiserlab','dodit'],
+  getMembers(){
+    this.members.map((name) => {
+      console.log(this);
+    });
+  }
+};
+
+SekolahKoding.getMembers();
+
+// ===============================================================================
+//var vs let vs const
+/*const person = {
   id : 1,
   name : 'Yusup'
 };
 
 person.age = 21;
 
-console.log(person);
+console.log(person);*/
 // var nama = "yusup"; //sifatnya function scope
 // let dan const sifatnya block scope
 // const tidak bisa di reassign (tp bukan immutable).
